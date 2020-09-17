9f235b50c3cc1ca19290fb26788bac5c6d942277
khermalik <khermalik@gmail.com>
2020-09-16 20:06:25 -0700

    data types

---
+++ scripts/app.js
@@ -0,0 +1,46 @@
+// 1 // ---------------------------- Data Types //
+
+// A light switch that can be either on or off
+// Data type = Boolean 
+'on' === true
+'off' === false
+
+// A user's email address
+// Data type = string
+const email = 'khermalik@gmail.com'
+
+// A spaceship with a hull, laser blaster, tractor beam, and warp drive.
+// Data type = Array
+const spaceshipFeatures = ['hull', 'laserblaster', 'tractor beam', 'warp drive']
+
+// A list of student names from our class.
+// Data type = Array
+const roster = ['Jim', 'Pam', 'Dwight']
+
+// A list of student names from our class, each with a location.
+// Data type = Object
+const locationRoster = {
+  Jim: 'Stamford',
+  Pam: 'Scranton',
+  Dwight: 'Schrute Farms',
+}
+
+// A list of student names from our class, each with a location and each with a list of favorite tv shows.
+// Data type = an Array of Objects
+const infoRoster = [{
+  name: 'Jim',
+  location: 'Stamford',
+  favTvShows: ['30 for 30', 'The IT Crowd'],
+  },
+  {
+  name: 'Pam',
+  location: 'Stamford',
+  favTvShows: ['Breaking Bad', 'Game of Thrones'],
+  }
+  {
+  name: 'Dwight',
+  location: 'Stamford',
+  favTvShows: ['Sherlock Holmes', 'Survivor'],
+  }
+]
+
