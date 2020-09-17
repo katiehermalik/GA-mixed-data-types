1b6d2a4f71867882d86d06716c6272618e5b37eb
khermalik <khermalik@gmail.com>
2020-09-16 20:58:14 -0700

    object-ception

--- scripts/app.js
+++ scripts/app.js
@@ -104,3 +104,27 @@ console.log(crazyObject.larry.characters[2].favouriteHobby);
 console.log(crazyObject.larry.nicknames[1]);
 // The object that contains the name funkhauser
 console.log(crazyObject.larry.characters[1]);
+
+// 4 // ------------------------- Object-ception //
+
+const inception = {
+  reality: {
+      dreamLayer1: {
+          dreamLayer2: {
+              dreamLayer3: {
+                  dreamLayer4: {
+                      dreamLayer5: {
+                          dreamLayer6: {
+                              limbo: "Joseph Gordon Levitt"
+                         }
+                      }
+                  }
+              }
+          }
+      }
+  }
+}
+
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
+console.log(inception);
+
